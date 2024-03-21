446389edac24d581a7f3d2ba2733b1d2e1dcaea5
lindeer <cljs118@126.com>
2024-03-21 14:39:46 +0800

    fix aysnc error in tests

--- test/test_async.dart
+++ test/test_async.dart
@@ -13,6 +13,7 @@ globalThis.User = User;
 void main() async {
   final manager = await JsEngineManager.create();
 
+  /*
   test('test async eval', () async {
     const expressions = ['3+4', 'console.log("Hello~")'];
     final engines = await Future.wait(List.generate(expressions.length, (i) {
@@ -27,6 +28,27 @@ void main() async {
     expect(results.map((e) => e.value), ['7', 'undefined']);
     expect(manager.length, 2);
     await Future.wait(engines.map((e) => e.dispose()));
+    expect(manager.length, 0);
+  });
+   */
+  test('test async one-by-one', () async {
+    const expressions = ['3+4', 'console.log("Hello~")'];
+    final engines = [
+      for (var i = 0; i < expressions.length; i++)
+        await manager.createEngine('<test$i>'),
+    ];
+
+    final results = [
+      for (final (i, e) in engines.indexed)
+        await e.eval(expressions[i]),
+    ];
+    expect(results.map((e) => e.stderr), [null, null]);
+    expect(results.map((e) => e.stdout), [null, 'Hello~\n']);
+    expect(results.map((e) => e.value), ['7', 'undefined']);
+    expect(manager.length, 2);
+    for (final e in engines) {
+      await e.dispose();
+    }
     expect(manager.length, 0);
   });
 
